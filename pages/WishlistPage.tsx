ebb56ee77c90c1dd209b08bac635a7e6a8b28459
rosalee-bd <rosaleebd@gmail.com>
2025-10-06 02:32:35 +0600

    Create WishlistPage.tsx

---
+++ pages/WishlistPage.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+const WishlistPage: React.FC = () => {
+  return null; // This page is intentionally left blank as the wishlist feature has been removed.
+};
+
+export default WishlistPage;
