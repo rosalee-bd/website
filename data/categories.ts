829c571eb027e473df7a46602d979476a3c3b32a
rosalee-bd <rosaleebd@gmail.com>
2025-10-06 02:28:24 +0600

    Create categories.ts

---
+++ data/categories.ts
@@ -0,0 +1,32 @@
+
+export interface CategoryInfo {
+  name: string;
+  description: string;
+}
+
+export const categoryDetails: CategoryInfo[] = [
+  {
+    name: 'All Categories',
+    description: 'Explore our full collection of the latest fashion trends and timeless pieces.'
+  },
+  {
+    name: 'Drop Tees',
+    description: 'Check out our latest drop of exclusive, limited-edition tees featuring unique designs and premium materials.'
+  },
+  {
+    name: 'Hoodies',
+    description: 'Find your perfect hoodie from our collection of cozy and stylish options, ideal for comfort and casual wear.'
+  },
+  {
+    name: 'Shirts',
+    description: 'Discover our collection of stylish and versatile shirts, perfect for every occasion from casual outings to formal events.'
+  },
+  {
+    name: 'Sweatshirts',
+    description: 'Browse our range of comfortable and fashionable sweatshirts, a perfect addition to your everyday wardrobe.'
+  },
+  {
+    name: 'Panjabi',
+    description: 'Explore our exclusive Mahra collection of elegant and traditional Panjabis, crafted for festive occasions and cultural celebrations.'
+  }
+];
